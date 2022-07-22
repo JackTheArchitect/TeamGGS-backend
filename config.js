0975b1998a6c439db938915c98caf50da53d06db
JackTheArchitect <jkim487@my.centennialcollege.ca>
2022-07-21 21:09:09 -0400

    user model schema and etc

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {        
+    "secret": "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzNzc1MTEzMywiaWF0IjoxNjM3NzUxMTMzfQ.gEcIRe4VUSYIi3sQpcQqhCEePC5f-G2db1D2iviGHPA",
+    "pwSecret" : "7yX$<m=hv0|fpjq",
+    mongodbUri : "mongodb+srv://admin:I7xiYmSvZMZaUwOh@cluster0.ovao6l8.mongodb.net/?retryWrites=true&w=majority"    
+}
